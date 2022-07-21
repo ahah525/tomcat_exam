72fce1aea320fb31f5035cda40fd9871014988e3
ahah525 <ahah525@naver.com>
2022-07-21 16:32:58 +0900

    GugudanServelt model 2 방식 적용

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -1,5 +1,6 @@
 package com.ll.exam;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,10 +19,12 @@ public class GugudanServlet extends HttpServlet {
         int dan = rq.getIntParam("dan", 0);
         int limit = rq.getIntParam("limit", 0);
 
-        rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+        // request에 정보를 담기(jsp에서 사용할 수 있도록)
+        req.setAttribute("dan", dan);
+        req.setAttribute("limit", limit);
 
-        for (int i = 1; i <= limit; i++) {
-            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan * i));
-        }
+        // html 로직 처리는 jsp에 토스
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/gugudan2.jsp");
+        requestDispatcher.forward(req, resp);
     }
 }
